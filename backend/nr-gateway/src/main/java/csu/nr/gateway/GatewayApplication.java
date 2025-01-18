d7839357e09349492b241a0790d1609f37df5233
yang <yangeureka@163.com>
2025-01-18 22:59:23 +0800

    update gateway

--- backend/nr-gateway/src/main/java/csu/nr/gateway/GatewayApplication.java
+++ backend/nr-gateway/src/main/java/csu/nr/gateway/GatewayApplication.java
@@ -1,2 +1,12 @@
-package csu.nr.gateway;public class GatewayApplication {
+package csu.nr.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
+
